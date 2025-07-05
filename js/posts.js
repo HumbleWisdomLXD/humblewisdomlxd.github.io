00ba610398cf1266a654f96797a3af8f109649d8
HumbleWisdomLXD <HopeWalker313@proton.me>
2025-07-05 15:51:52 -0500

    Create posts.js

---
+++ js/posts.js
@@ -0,0 +1,13 @@
+const blogPosts = [
+    {
+        id: 'first-post',
+        title: 'My First Blog Post!',
+        excerpt: 'This is the beginning of my blogging journey.',
+        content: '<p>Hello world! Welcome to my new blog.</p>',
+        category: 'General',
+        date: '2023-11-01',
+        author: 'You',
+        readTime: 2,
+        featured: true
+    }
+];
