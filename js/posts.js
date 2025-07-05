c324396793c372075dbcfcd52fc1e3427acefa37
HumbleWisdomLXD <HopeWalker313@proton.me>
2025-07-05 16:05:24 -0500

    Update posts.js

--- js/posts.js
+++ js/posts.js
@@ -2,23 +2,21 @@ const blogPosts = [
     {
         id: 'first-post',
         title: 'My First Blog Post!',
-        excerpt: 'This is the beginning of my blogging journey.',
         content: '<p>Hello world! Welcome to my new blog.</p>',
         category: 'General',
         date: '2023-11-01',
         author: 'You',
         readTime: 2,
         featured: true
-    }
-];
-{
-    id: 'second-post',
-    title: 'My Second Post!',
-    excerpt: 'This is another test post.',
-    content: '<p>More content here. It works!</p>',
-    category: 'Updates',
-    date: '2023-11-02',
-    author: 'You',
-    readTime: 1,
-    featured: true
-},
+    },  // <-- COMMA here (not semicolon)
+    {
+        id: 'second-post',
+        title: 'My Second Post!',
+        content: '<p>More content here. It works!</p>',
+        category: 'Updates',
+        date: '2023-11-02',
+        author: 'You',
+        readTime: 1,
+        featured: true
+    }   // <-- NO comma after the last post
+];      // <-- Semicolon ONLY at the very end
